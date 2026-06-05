1ce6371abe015dff58f562f95823a8fb30b780ec
YasmimCruzEng <202602569418@alunos.estacio.br>
2026-06-04 22:39:48 -0300

    Questão 01
    Questão 1 – Cadastro Simples (0,2 ponto)
    Crie um programa que armazene:
    •	nome;
    •	idade;
    •	altura.
    Depois exiba todas as informações utilizando print().

---
+++ Questão 01.py
@@ -0,0 +1,10 @@
+#Primeiro se utiliza o "inpunt" para que o usuário digite as informações necessárias
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura: ")
+
+#Com o comando print e informando qual a informação, se mostra as informações digitadas
+print("\n Informações inseridas:")
+print(f"Nome informado: {nome}")
+print(f"Idade informada: {idade}")
+print(f"Altura informado: {altura}")
